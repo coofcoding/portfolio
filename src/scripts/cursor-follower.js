ef505c61ecf285a012e2097cbd192d9888404be6
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-15 19:57:31 -0500

    fix bug with the cursor effect

--- src/scripts/cursor-follower.js
+++ src/scripts/cursor-follower.js
@@ -1,4 +1,5 @@
 document.addEventListener("mousemove", function (e) {    
-    document.querySelector('#_cursorFollows').style.transform = `translate(calc(${e.clientX}px - 200px), calc(${e.clientY}px - 180px))`;
-    document.querySelector('#_pointerTexture').style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`;
+    console.log(e)
+    document.querySelector('#_cursorFollows').style.transform = `translate(calc(${e.layerX}px - 200px), calc(${e.layerY}px - 180px))`;
+    document.querySelector('#_pointerTexture').style.transform = `translate(calc(${e.layerX}px - 50%), calc(${e.layerY}px - 50%))`;
 });
