c0cd7b47e7660d60239c3d8c310438192bb589da
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-16 20:33:09 -0500

    adding whoami content and modifying some sizes for more responsive

--- src/scripts/cursor-follower.js
+++ src/scripts/cursor-follower.js
@@ -1,5 +1,4 @@
 document.addEventListener("mousemove", function (e) {    
-    console.log(e)
     document.querySelector('#_cursorFollows').style.transform = `translate(calc(${e.layerX}px - 200px), calc(${e.layerY}px - 180px))`;
     document.querySelector('#_pointerTexture').style.transform = `translate(calc(${e.layerX}px - 50%), calc(${e.layerY}px - 50%))`;
 });
