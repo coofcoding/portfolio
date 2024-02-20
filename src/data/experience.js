e19dbcd54b418d5e183019b2f88676706b4aecc3
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-19 20:24:01 -0500

    experience component added

---
+++ src/data/experience.js
@@ -0,0 +1,30 @@
+export const Experiences = [
+    {
+        type: 'Business practice',
+        date: 'September 2023 - January 2024',
+        title: 'INNOVENTION',
+        subtitle: 'General Assistant',
+        description: "The main objetive was restore the official company website and develop interactive web videogame called 'Knight tour' for the childrens and then, they will can improve their stategical mind and their thinking skills.",
+    },
+    {
+        type: 'University Career',
+        date: 'January 2023 - November 2023',
+        title: 'UNIVERSIDAD AUTONOMA DE MANIZALES',
+        subtitle: 'TECHNOLOGIST IN ANALYSIS AND PROGRAMMING OF INFORMATION SYSTEMS',
+        description: 'TECNOLOGÍA EN ANALISIS Y PROGRAMACIÓN DE SISTEMAS DE INFORMACIÓN',
+    },
+    {
+        type: 'University Career',
+        date: 'January 2022 - November 2022',
+        title: 'UNIVERSIDAD AUTONOMA DE MANIZALES',
+        subtitle: 'PROFESSIONAL TECHNICIAN IN COMPUTER PROGRAMMING',
+        description: 'TECNICO PROFESIONAL EN PROGRAMACION DE COMPUTADORES',
+    },
+    {
+        type: 'University Career',
+        date: 'January 2021 - November 2021',
+        title: 'UNIVERSIDAD AUTONOMA DE MANIZALES',
+        subtitle: 'LABOR ASSISTANT TECHNICIAN IN COMPUTER SYSTEMS',
+        description: 'TECNICO LABORAL AUXILIAR EN SISTEMAS INFORMATICOS',
+    }
+] 
