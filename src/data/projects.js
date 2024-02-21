50270187a852596ec265007fffe2fa49715b53e5
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-21 17:27:11 -0500

    color frameworks pills and mobile responsive fixed

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,56 +8,38 @@ export const ProjectsDeveloped = [
             {
                 frameworkName: 'Angular',
                 img: '../../public/frameworks/angular.svg',
-                color: '#9D334B',
-                bgColor: '#1F0107'
             },
             {
                 frameworkName: 'XAMPP',
                 img: '../../public/frameworks/xampp.svg',
-                color: '#FB7A24',
-                bgColor: '#1F0D00'
             },
             {
                 frameworkName: 'Bootstrap',
                 img: '../../public/frameworks/bootstrap.svg',
-                color: '#7952B3',
-                bgColor: '#110129'
             },
             {
                 frameworkName: 'MySQL',
                 img: '../../public/frameworks/mysql.svg',
-                color: '#4479A1',
-                bgColor: '#001627'
             },
             {
                 frameworkName: 'django',
                 img: '../../public/frameworks/django.svg',
-                color: '#136949',
-                bgColor: '#00150D'
             },
             {
                 frameworkName: 'Typescript',
                 img: '../../public/frameworks/typescript.svg',
-                color: '#3178C6',
-                bgColor: '#011933'
             },
             {
                 frameworkName: 'HTML',
                 img: '../../public/frameworks/html.svg',
-                color: '#E34F26',
-                bgColor: '#360C01'
             },
             {
                 frameworkName: 'CSS',
                 img: '../../public/frameworks/css.svg',
-                color: '#1572B6',
-                bgColor: '#01253F'
             },
             {
                 frameworkName: 'Python',
                 img: '../../public/frameworks/python.svg',
-                color: '#FFD243',
-                bgColor: '#271E00'
             }
         ]
     },
@@ -70,50 +52,34 @@ export const ProjectsDeveloped = [
             {
                 frameworkName: 'Angular',
                 img: '../../public/frameworks/angular.svg',
-                color: '#9D334B',
-                bgColor: '#1F0107'
             },
             {
                 frameworkName: 'XAMPP',
                 img: '../../public/frameworks/xampp.svg',
-                color: '#FB7A24',
-                bgColor: '#1F0D00'
             },
             {
                 frameworkName: 'Ionic',
                 img: '../../public/frameworks/ionic.svg',
-                color: '#3880FF',
-                bgColor: '#001335'
             },
             {
                 frameworkName: 'MySQL',
                 img: '../../public/frameworks/mysql.svg',
-                color: '#4479A1',
-                bgColor: '#001627'
             },
             {
                 frameworkName: 'Deezer API',
                 img: '../../public/frameworks/deezer.svg',
-                color: '#231F20',
-                bgColor: '#0E0E0E'
             },
             {
                 frameworkName: 'Typescript',
                 img: '../../public/frameworks/typescript.svg',
-                color: '#3178C6',
-                bgColor: '#011933'
             },
             {
                 frameworkName: 'HTML',
                 img: '../../public/frameworks/html.svg',
-                color: '#E34F26',
-                bgColor: '#360C01'
             },
             {
                 frameworkName: 'CSS',
                 img: '../../public/frameworks/css.svg',
-                color: '#1572B6',
-                bgColor: '#01253F'
             },
         ]
     },
