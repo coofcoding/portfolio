2709e5b7873ace7070ff13287fe0043d3e5d9391
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-20 21:55:22 -0500

    images optimization, initializing projects component (bug with tailwind classes)

---
+++ src/data/projects.js
@@ -0,0 +1,120 @@
+export const ProjectsDeveloped = [
+    {
+        name: "onTask's",
+        description:
+            "Was developed as final project on the subject Dynamic web pages, and at the same time, recognized as one of the best's projects in the LABOR ASSISTANT TECHNICIAN IN COMPUTER SYSTEMS projects fair.",
+        exampleImg: '../../public/projectsPreview/ontasks.avif',
+        frameworks: [
+            {
+                frameworkName: 'Angular',
+                img: '../../public/frameworks/angular.svg',
+                color: '#9D334B',
+                bgColor: '#1F0107'
+            },
+            {
+                frameworkName: 'XAMPP',
+                img: '../../public/frameworks/xampp.svg',
+                color: '#FB7A24',
+                bgColor: '#1F0D00'
+            },
+            {
+                frameworkName: 'Bootstrap',
+                img: '../../public/frameworks/bootstrap.svg',
+                color: '#7952B3',
+                bgColor: '#110129'
+            },
+            {
+                frameworkName: 'MySQL',
+                img: '../../public/frameworks/mysql.svg',
+                color: '#4479A1',
+                bgColor: '#001627'
+            },
+            {
+                frameworkName: 'django',
+                img: '../../public/frameworks/django.svg',
+                color: '#136949',
+                bgColor: '#00150D'
+            },
+            {
+                frameworkName: 'Typescript',
+                img: '../../public/frameworks/typescript.svg',
+                color: '#3178C6',
+                bgColor: '#011933'
+            },
+            {
+                frameworkName: 'HTML',
+                img: '../../public/frameworks/html.svg',
+                color: '#E34F26',
+                bgColor: '#360C01'
+            },
+            {
+                frameworkName: 'CSS',
+                img: '../../public/frameworks/css.svg',
+                color: '#1572B6',
+                bgColor: '#01253F'
+            },
+            {
+                frameworkName: 'Python',
+                img: '../../public/frameworks/python.svg',
+                color: '#FFD243',
+                bgColor: '#271E00'
+            }
+        ]
+    },
+    {
+        name: "Stofyty -plus",
+        description:
+            "Was developed as final project with the base of create a Spotify clone but using the Deezer API on the subject Development platforms, the design was created with fully native CSS and recognized as the BEST project between my classmates.",
+        exampleImg: "../../public/projectsPreview/stofyty.avif",
+        frameworks: [
+            {
+                frameworkName: 'Angular',
+                img: '../../public/frameworks/angular.svg',
+                color: '#9D334B',
+                bgColor: '#1F0107'
+            },
+            {
+                frameworkName: 'XAMPP',
+                img: '../../public/frameworks/xampp.svg',
+                color: '#FB7A24',
+                bgColor: '#1F0D00'
+            },
+            {
+                frameworkName: 'Ionic',
+                img: '../../public/frameworks/ionic.svg',
+                color: '#3880FF',
+                bgColor: '#001335'
+            },
+            {
+                frameworkName: 'MySQL',
+                img: '../../public/frameworks/mysql.svg',
+                color: '#4479A1',
+                bgColor: '#001627'
+            },
+            {
+                frameworkName: 'Deezer API',
+                img: '../../public/frameworks/deezer.svg',
+                color: '#231F20',
+                bgColor: '#0E0E0E'
+            },
+            {
+                frameworkName: 'Typescript',
+                img: '../../public/frameworks/typescript.svg',
+                color: '#3178C6',
+                bgColor: '#011933'
+            },
+            {
+                frameworkName: 'HTML',
+                img: '../../public/frameworks/html.svg',
+                color: '#E34F26',
+                bgColor: '#360C01'
+            },
+            {
+                frameworkName: 'CSS',
+                img: '../../public/frameworks/css.svg',
+                color: '#1572B6',
+                bgColor: '#01253F'
+            },
+        ]
+    },
+];
