d84cb4b93b9002bdb6336993d6d6dfcb7e01331f
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-23 18:14:51 -0500

    responsive background gradient fixed

--- src/data/frameworks.js
+++ src/data/frameworks.js
@@ -2,76 +2,76 @@ export const frameworks = [
     {
         name: 'HTML5',
         icon: '../../public/technologies/html.avif',
-        class: "col-span-11 md:col-span-3"
+        class: "col-span-11 lg:col-span-3"
     },
     {
         name: 'CSS3',
         icon: '../../public/technologies/css.avif',
-        class: "col-span-11 md:col-span-3"
+        class: "col-span-11 lg:col-span-3"
     },
     {
         name: 'FIGMA',
         icon: '../../public/technologies/figma.avif',
-        class: "col-span-11 md:col-span-5"
+        class: "col-span-11 lg:col-span-5"
     },
     {
         name: 'JAVASCRIPT',
         icon: '../../public/technologies/js.avif',
-        class: "col-span-11 md:col-span-5"
+        class: "col-span-11 lg:col-span-5"
     },
     {
         name: 'VITE',
         icon: '../../public/technologies/vite.avif',
-        class: "col-span-11 md:col-span-2"
+        class: "col-span-11 lg:col-span-2"
     },
     {
         name: 'REACT REDUX TOOLKIT',
         icon: '../../public/technologies/rrt.avif',
-        class: "col-span-11 md:col-span-4 md:row-span-2"
+        class: "col-span-11 lg:col-span-4 lg:row-span-2"
     },
     {
         name: 'REACTJS',
         icon: '../../public/technologies/react.avif',
-        class: "col-span-11 md:col-span-4"
+        class: "col-span-11 lg:col-span-4"
     },
     {
         name: 'PNPM',
         icon: '../../public/technologies/pnpm.avif',
-        class: "col-span-11 md:col-span-3"
+        class: "col-span-11 lg:col-span-3"
     },
     {
         name: 'NODE.JS',
         icon: '../../public/technologies/nodejs.avif',
-        class: "col-span-11 md:col-span-5"
+        class: "col-span-11 lg:col-span-5"
     },
     {
         name: 'TAILWIND CSS',
         icon: '../../public/technologies/tailwind.avif',
-        class: "col-span-11 md:col-span-6"
+        class: "col-span-11 lg:col-span-6"
     },
     {
         name: 'GIT',
         icon: '../../public/technologies/git.avif',
-        class: "col-span-11 md:col-span-4 md:row-span-2"
+        class: "col-span-11 lg:col-span-4 lg:row-span-2"
     },
     {
         name: 'VISUAL STUDIO CODE',
         icon: '../../public/technologies/vscode.avif',
-        class: "col-span-11 md:col-span-7"
+        class: "col-span-11 lg:col-span-7"
     },
     {
         name: 'VISUAL STUDIO',
         icon: '../../public/technologies/vs.avif',
-        class: "col-span-11 md:col-span-7"
+        class: "col-span-11 lg:col-span-7"
     },
     {
         name: 'GITHUB',
         icon: '../../public/technologies/github.avif',
-        class: "col-span-11 md:col-span-6"
+        class: "col-span-11 lg:col-span-6"
     },
     {
         name: 'POSTMAN',
         icon: '../../public/technologies/postman.avif',
-        class: "col-span-11 md:col-span-5"
+        class: "col-span-11 lg:col-span-5"
     },
 ]
