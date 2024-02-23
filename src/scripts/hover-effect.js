b8d6a5dd6bd8e5f3f73dfd8b7054ed9522e991e6
coofcoding <155861737+coofcoding@users.noreply.github.com>
2024-02-23 18:50:38 -0500

    contactme component created

---
+++ src/scripts/hover-effect.js
@@ -0,0 +1,100 @@
+const _twitterHover = document.getElementById("_twitterHover");
+const _gmailHover = document.getElementById("_gmailHover");
+const _linkedinHover = document.getElementById("_linkedinHover");
+const _githubHover = document.getElementById("_githubHover");
+const _igHover = document.getElementById("_igHover");
+
+_twitterHover.addEventListener("mouseover", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.add('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.remove('backdrop-blur-md');
+
+})
+
+_twitterHover.addEventListener("mouseleave", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.remove('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.add('backdrop-blur-md');
+
+
+})
+_gmailHover.addEventListener("mouseover", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.add('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.remove('backdrop-blur-md');
+
+
+})
+
+_gmailHover.addEventListener("mouseleave", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.remove('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.add('backdrop-blur-md');
+
+
+})
+_linkedinHover.addEventListener("mouseover", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.add('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.remove('backdrop-blur-md');
+
+
+})
+
+_linkedinHover.addEventListener("mouseleave", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.remove('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.add('backdrop-blur-md');
+
+
+})
+_githubHover.addEventListener("mouseover", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.add('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.remove('backdrop-blur-md');
+
+
+})
+
+_githubHover.addEventListener("mouseleave", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.remove('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.add('backdrop-blur-md');
+
+
+})
+_igHover.addEventListener("mouseover", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.add('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.remove('backdrop-blur-md');
+
+
+})
+
+_igHover.addEventListener("mouseleave", () => {
+    
+    const _pointerTexture = document.getElementById("_pointerTexture");
+
+    _pointerTexture.classList.remove('w-36', 'h-36', 'bg-white', 'mix-blend-difference');
+    _pointerTexture.classList.add('backdrop-blur-md');
+
+
+})
